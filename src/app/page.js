69a0df0c5b1049c0ccd167970cc02bd4ef7d4364
nargis21 <nargisakther1933@gmail.com>
2023-10-03 23:13:35 +0600

    dynamic data fetching and ssr

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ import Products from "@/components/UI/Products";
 // }
 
 const HomePage = async () => {
-  const res = await fetch('http://localhost:5000/watches', { cache: 'force-cache', next: { revalidate: 5 } })
+  const res = await fetch('http://localhost:5000/watches', { cache: 'force-cache', next: { revalidate: 1 } })
   const data = await res.json()
   // const data = await getData()
   return (
